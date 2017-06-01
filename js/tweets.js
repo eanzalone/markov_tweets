dff9d3c94623963acdbea62f2cbf02e03bf5b0e2
eanzalone <emily.anzalone@gmail.com>
2017-06-01 16:03:21 -0700

    Connected to Twitter

--- js/tweets.js
+++ js/tweets.js
@@ -1,11 +1,9 @@
 console.log("Tweets being gathered...");
 
 var Twit = require('twit');
+var config = require('./config');
 
-var T = new Twit({
-  consumer_key:         'd5UiqoVCaWDEwvj0Zp6Fp1pKx',
-  consumer_secret:      'G6N3YPkH1ZDVDUHhLosZkTD64EWJePEvRLGFzSCkNQraUbWrwv',
-  access_token:         '3740578213-Uw7sXt23M4gpYWEvOgaXI8unDccNN63SNrYMdVN',
-  access_token_secret:  'Jv35JcpsByZv7uTs6GIBkfRyoSO21Q22ARHiKtHAPxHbB'
-})
+var T = new Twit(config);
+
+console.log("Tweets gathered!")
 
