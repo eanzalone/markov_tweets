8f112aa843deaf20ed8fed5788e9d895b831dc9b
eanzalone <emily.anzalone@gmail.com>
2017-06-01 17:23:43 -0700

    able to get user tweets - harcoded

--- js/tweets.js
+++ js/tweets.js
@@ -1,9 +1,27 @@
 console.log("Tweets being gathered...");
-
+// REQUIREMENTS
 var Twit = require('twit');
 var config = require('./config');
 
+// GLOBAL VARIABLES
 var T = new Twit(config);
+var twitterHandle = 'cptmidas';
+var params = {
+    screen_name: twitterHandle,
+    lang: 'en',
+    // exclude_replies: true,
+    count: 5
+}
 
-console.log("Tweets gathered!")
+// GET REQUESTS
+T.get('statuses/user_timeline', params, gotUserTweets);
 
+
+// DISPLAY FUNCTIONS
+function gotUserTweets(err, data, response) {
+    for (var i = 0; i < data.length; i++) {
+        console.log(data[i].created_at);
+        console.log(data[i].text);
+        console.log('-----------------------------');
+    }
+};
